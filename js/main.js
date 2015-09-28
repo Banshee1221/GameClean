6818dccd497af2d43c3a4b8c28c13657b5927c7d
Eugene <banshee1221@gmail.com>
2015-09-29 00:07:12 +0200

    Changes to identification and display

--- js/main.js
+++ js/main.js
@@ -3,61 +3,82 @@ var proc = require('process');
 var recursive = require('recursive-readdir');
 var minmat = require('minimatch')
 var fileTypes = require('./js/commonFiles.json')
+var paths = require('path');
 
 function getDirContents(number){
     var directory = document.getElementById('steamDir').value;
     console.log("after dir");
     if (number == 1){
-         var tempArr;
-        if (proc.platform == "linux"){
-            console.log("Got Linux!");
-            tempArr = directory.split("/");
-        }
-        else if (proc.platform == "win32"){
-            console.log("Got Winbloze!");
-            tempArr = directory.split("\\");
-        }
+        var tempArr = directory.split(paths.sep);
         console.log(tempArr);
         lastEle = tempArr[tempArr.length - 1];
-        if (lastEle != "SteamApps" && lastEle != "steamapps" && lastEle != "steamApps" && lastEle != "Steamapps"){
+        if (lastEle.toLowerCase() != "steamapps"){
             alert("Please select a proper SteamApps directory!");
             document.getElementById('checklist').innerHTML = "";
             return console.error("Not an appropriate directory");
         }
+        console.log("Directory passed to itemParser: "+directory)
         itemParser(directory);
     }
 };
 
 function itemParser(directory){
     //console.error(directory);
+    var games = [];
     recursive(directory, function(err, files){
         if (err) {
             return console.error(err);
         }
-        var count = 0
+        var count = 0;
         var fileChecks = fileTypes['redistFiles'];
+        var ind1 = -1;
         console.log(fileTypes['redistFiles']);
         files.forEach(function(file){
+            var tmp = file.split(paths.sep);
+            if (ind1 == -1) {
+                if (tmp.indexOf("steamapps") != -1){
+                    ind1 = tmp.indexOf("steamapps");
+                }
+                else if (tmp.indexOf("SteamApps") != -1){
+                    ind1 = tmp.indexOf("SteamApps");
+                }
+                else if (tmp.indexOf("steamApps") != -1){
+                    ind1 = tmp.indexOf("steamApps");
+                }
+                else if (tmp.indexOf("Steamapps") != -1){
+                    ind1 = tmp.indexOf("Steamapps");
+                }
+                else if (tmp.indexOf("STEAMAPPS") != -1){
+                    ind1 = tmp.indexOf("STEAMAPPS");
+                }
+                console.log(ind1);
+            }
+            if (tmp[ind1+1] == "common"){
+                if (games.indexOf(tmp[ind1+2]) < 0){
+                    games.push(tmp[ind1+2]);
+                }
+            }
+        });
+        games.forEach(function(game){
+            var tmp = document.createElement("div");
+            tmp.setAttribute("id", game);
+            tmp.innerHTML = "<h4>"+game+"</h4>";
+            document.getElementById('checklist').appendChild(tmp);
+        });
+        files.forEach(function(file){
+            var tmp = file.split(paths.sep);
+            var toWrite = tmp.slice(tmp.indexOf(tmp[ind1+2]) + 1, tmp.length).join(paths.sep);
+            //console.log(toWrite);
             fileChecks.forEach(function(item){
                 var test = minmat(file, item['pat'], {matchBase: true, nocase: true});
                 if (test) {
-                    var tmp = document.createElement("div");
-                    tmp.innerHTML = "<p><input type='checkbox' id='"+count.toString()+"'><label class='radioClass' for='"+count.toString()+"'>"+file+"</label></input></p>"
-                    /*var tmp1 = tmp.appendChild(document.createElement("P"));
-                    var tmp2 = tmp1.appendChild(document.createElement("input"));
-                    tmp2.setAttribute("class", "with-gap");
-                    tmp2.setAttribute("name", "group");
-                    tmp2.setAttribute("type", "radio");
-                    tmp2.setAttribute("id", count.toString())
-                    var tmp3 = tmp2.appendChild(document.createElement("label"));
-                    tmp3.setAttribute("for", count.toString())
-                    tmp3.innerHTML = file;*/
-                    document.getElementById('checklist').appendChild(tmp); //console.log(file);
+                    var tmpEl = document.createElement("div");
+                    tmpEl.innerHTML = "<p align='right'><input type='checkbox' id='"+count.toString()+"' checked><label class='radioClass' for='"+count.toString()+"'>"+toWrite+"</label></input></p>";
+                    document.getElementById(tmp[ind1+2]).appendChild(tmpEl); //console.log(file);
                     count++;
-                };
+                }
             });
-            //console.error('<div>'+file+'</div>');
         });
-        //document.getElementById('contentsOf').appendChild('<div>'+file+'</div>');
+        console.log(games);
     });
 };
